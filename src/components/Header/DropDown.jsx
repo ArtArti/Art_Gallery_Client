f85c3e441dacc767e69e5298fd70a09502330abd
Arti <singharti7648@gmail.com>
2025-06-10 09:32:09 +0530

    addedd drop down

--- src/components/Header/DropDown.jsx
+++ src/components/Header/DropDown.jsx
@@ -3,35 +3,30 @@ import { useAuth } from '../../AuthContect/AuthContext';
 
 export default function DropDown() {
   const navigate = useNavigate();
-  const {setUser, setIsLoggedIn} = useAuth();
+  const { setUser, setIsLoggedIn } = useAuth();
 
   const handleChange = (e) => {
     const path = e.target.value;
-    if (path) {
+
+    if (path === 'logout') {
+      localStorage.removeItem("token");
+      setIsLoggedIn(false);
+      setUser(null);
+      navigate("/");
+    } else if (path) {
       navigate(path);
     }
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setIsLoggedIn(false);
-    setUser(null);
-      navigate("/");
-  };
-
-
-
   return (
     <select
       onChange={handleChange}
-      className="items-center space-x-1 border border-amber-600 rounded-md px-1 py-1 "
+      className="items-center space-x-1 border border-amber-600 rounded-md px-1 py-1"
     >
       <option value="">Select</option>
-      <option value="#changePassword">Change Password</option>
-      <option value="#contact">Contact</option>
-      <option onClick={handleLogout}
-       className="text-sm text-red-600 hover:text-red-800 font-medium"
-      >LogOut</option>
+      <option value="/change-password">Change Password</option>
+      <option value="/contact">Contact</option>
+      <option value="logout" className="text-red-600">Log Out</option>
     </select>
   );
 }
